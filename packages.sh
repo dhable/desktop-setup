577132603f575eb3db3e2b344954866293a91474
Dan Hable <418679+dhable@users.noreply.github.com>
2025-01-16 11:05:50 -0600

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,6 +2,10 @@
 
 # Install homebrew and packages
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> /Users/dhable/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/dhable/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+    
 brew install autoconf awk bat cmake ctop diff-so-fancy fish fzf gh git htop jq lazydocker \
 	neovim kubectl terraform perf typescript go
 brew install --cask iterm2 
