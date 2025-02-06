2f4b80119d42b0e69a30e6e9dd7d77dcf0ce593e
Dan Hable <418679+dhable@users.noreply.github.com>
2025-02-06 09:55:26 -0600

    Update git-setup.fish

--- git-setup.fish
+++ git-setup.fish
@@ -2,3 +2,8 @@
 
 git config --global user.name "Dan Hable"
 git config --global user.email "dan@danhable.com"
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RF"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
+git config --bool --global diff-so-fancy.stripLeadingSymbols false
+git config --bool --global diff-so-fancy.markEmptyLines false
+git config --global diff-so-fancy.rulerWidth 120
