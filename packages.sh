840f24853e002b111e1e5592a26fc08845b4cad7
Dan Hable <418679+dhable@users.noreply.github.com>
2025-03-17 13:47:42 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -8,11 +8,4 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
     
 brew install autoconf awk bat cmake ctop diff-so-fancy fish fzf gh git htop jq lazydocker \
 	neovim kubectl terraform perf typescript go helm
-brew install --cask iterm2 joplin cursor docker 
-
-# Setup iterm2 defaults
-defaults write com.googlecode.iterm2 QuitWhenAllWindowsClosed -int 0
-defaults write com.googlecode.iterm2 SUEnableAutomaticChecks -int 0
-defaults write com.googlecode.iterm2 SoundForEsc -int 0
-defaults write com.googlecode.iterm2 VisualIndicatorForEsc -int 0
-defaults write com.googlecode.iterm2 PromptOnQuit -int 1
+brew install --cask warp joplin cursor docker 
